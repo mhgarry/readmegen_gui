95f8aa631f1c8c894dcbd85c6f4384a6729b8f4c
Matthew Garry <mhgarry92@gmail.com>
2023-11-04 23:27:27 -0400

    set up apiRouter

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,10 +1,8 @@
 import { Router as router } from 'express';
 import showPrompts from './promptRoutes';
 
-const apiRouter = () => {
-router.use('/api', (req, res) => {
-    res.send(showPrompts)
-})
-}
+const apiRouter = router(); // router to communicate between the server and client side of my project. The glue of my api 
 
-export default apiRouter;
+apiRouter.use('/api', showPrompts); //defines the showPrompts function as an api route to pass to frontend
+
+export default apiRouter; // exported to be used in my main server and passed to my front end
