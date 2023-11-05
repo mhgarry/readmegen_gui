8b96305a1584e87862a271239129b776dc596aaf
Matthew Garry <mhgarry92@gmail.com>
2023-11-04 20:29:45 -0400

    init/psuedocode

---
+++ server/main.js
@@ -0,0 +1,13 @@
+// Use Node, Express, Inquirer, fs
+// generate our markdown file on backend based on the input from frontend
+// create an inquirer tree of questions to organize the questions/answers for us
+// create a way to generate and append badges to readme for licensing
+// create a function that writes the README.md file for the user after they're done answering their questions
+// serve the markdown file to the user to directly download to their computer
+
+// use React, axios, and ???
+// create GUI
+// create question headers
+// create answer boxes as forms
+// create submit answer button that communicates with the server and gives it the answer to the particular question to write on the README.md file
+// when questions are over make download link appear so the user can download their README.md file
